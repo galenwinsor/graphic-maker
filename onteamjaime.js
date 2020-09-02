e4100f00a85fb09eae3e0b751554894dba7836b3
galenlwinsor <70543966+galenlwinsor@users.noreply.github.com>
2020-09-02 16:33:34 -0400

    Update onteamjaime.js

--- onteamjaime.js
+++ onteamjaime.js
@@ -31,41 +31,6 @@ function expand(e) {
   document.getElementById(e).style.height = "auto";
 }
 
-// fetches an image from an api, creating a url object from that image blob
-// and inserting it into a img element
-function fetchImage(e) {
-  const myRequest = new Request('https://wlcs.wombat-labs.com/photoframer/upload', {
-    method: 'POST',
-    mode: 'cors',
-    body: new FormData(document.getElementById('avatar-form')),
-    redirect: 'follow'
-  });
-
-  const myImage = document.getElementById('avatar-generated');
-
-  document.getElementById('display-box').style.display = "flex";
-
-  fetch(myRequest)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.blob();
-    })
-    .then(myBlob => {
-      const url = URL.createObjectURL(myBlob);
-      myImage.src = url;
-      document.getElementById('download').href = url;
-    })
-    .catch(error => {
-      alert("Oops! There was an issue with your upload. Please try again.");
-      document.getElementById('display-box').style.zIndex = "-1";
-      console.error('There has been a problem with your fetch operation:', error);
-    });
-
-  return false;
-}
-
 function makeImage() {
         const canvas = document.getElementById('canvas');
         const ctx = canvas.getContext('2d');
@@ -74,7 +39,7 @@ function makeImage() {
         frame.src = 'images/JaimeCapeFinal.png';
         frame.onload = function() {
             ctx.drawImage(frame, 0, 0, 1200, 675);
-            upload = document.getElementById('file').files.item(0);
+            upload = document.getElementById('inputfile').files.item(0);
             image.src = URL.createObjectURL(upload);
             image.onload = function() {
               ctx.globalCompositeOperation = 'destination-over';
